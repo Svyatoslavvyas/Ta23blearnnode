8da5343c2d990ea8d0dbe952ee8877a0709e7d82
svyatoslav <svyatoslavvyasovoi@gmail.com>
2024-10-07 11:44:29 +0300

    Add es and common js modules

---
+++ index.js
@@ -0,0 +1,8 @@
+//const common=require('./common.js');
+//common.hello();
+//const es = require('./es.js').default;
+//es.hello();
+import common from './common.js';
+common.hello();
+import es from './es.js';
+es.hello();
