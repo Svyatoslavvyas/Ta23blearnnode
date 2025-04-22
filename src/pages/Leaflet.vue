6070df08db8fed1f4b10c6b38e2c4cf089ad52c7
svyatoslav <svyatoslavvyasovoi@gmail.com>
2025-04-22 14:15:13 +0300

    Create Leaflet.vue

---
+++ src/pages/Leaflet.vue
@@ -0,0 +1,13 @@
+<script setup>
+import L from 'leaflet';
+console.log()
+
+var map = L.map
+L.titleLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}) .addTo(map);
+</script>
+<template>
+    <div id="map"></div>
+</template>
