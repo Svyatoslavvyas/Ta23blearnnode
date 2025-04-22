59653cf543bc9a11e63bf0ece360d64046c0cb99
svyatoslav <svyatoslavvyasovoi@gmail.com>
2025-04-22 11:30:56 +0300

    Update Chat.vue

--- src/Chat.vue
+++ src/Chat.vue
@@ -4,38 +4,36 @@ import { ref } from 'vue';
 
 let message = ref('');
 let messages = ref([]);
-let lastMessageDate = null;
 
-let res = await axios.get('http://localhost:3000/messages');
-messages.value.push(...res.data);
-if(res.data.length > 0){
-    let lastMessage = res.data[res.data.length-1];
-    lastMessageDate = lastMessage.date;
-}
 
-longPoll();
+// let res = await axios.get('http://localhost:3000/messages');
+// messages.value.push(...res.data);
 
-async function longPoll(){
-    let res = await axios.get('http://localhost:3000/messages/longpoll', {
-        params: {
-            date: lastMessageDate
-        }
-    });
-    messages.value.push(...res.data);
-    if(res.data.length > 0){
-        let lastMessage = res.data[res.data.length-1];
-        lastMessageDate = lastMessage.date;
-    }
-    await longPoll();
-}
+// Create WebSocket connection.
+const socket = new WebSocket("ws://localhost:8080");
 
+// Connection opened
+socket.addEventListener("open", (event) => {
+  //socket.send("Hello Server!");
+});
+
+// Listen for messages
+socket.addEventListener("message", (event) => {
+  
+  let data = JSON.parse(event.data);
+  console.log(data);
+  if(data.type === 'messages') {
+    messages.value.push(...data.messages);
+  }
+  if(data.type === 'message') {
+    messages.value.push(data);
+  }
+});
 
 
 
 async function send() {
-    let res = await axios.post('http://localhost:3000/messages', {
-        message: message.value
-    });
+    socket.send(JSON.stringify({type: 'message', message: message.value}));
     message.value = '';
 }
 </script>
