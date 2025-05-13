c17699a507d4863c48bb784046c910740584ced6
svyatoslav <svyatoslavvyasovoi@gmail.com>
2025-05-13 11:43:29 +0300

    map

--- src/pages/Leaflet.vue
+++ src/pages/Leaflet.vue
@@ -1,13 +1,8 @@
 <script setup>
-import { ref } from 'vue';
-import LeafletMap from '../components/LeafletMap.vue';
+import LeafletMap from '../comp/LeafletMap.vue';
 
-let coords = ref([59.42691, 24.74344]);
-let zoom = ref(19);
+let cords = [59.4154040084724, 24.64667272153351]
 </script>
 <template>
-    <button class="button is-primary" @click="coords=[58.378003935892494, 26.727245723399832]">Go to Tartu</button>
-    <input type="range" min="1" max="19" step="1" v-model="zoom">
-    <LeafletMap :center="coords" :zoom="zoom"></LeafletMap>
-    <LeafletMap :center="[59.4332429007378, 24.75129553356808]" :zoom="10"></LeafletMap>
+    <LeafletMap :center="cords"></LeafletMap>
 </template>
