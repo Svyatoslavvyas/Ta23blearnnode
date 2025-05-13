21d00882797ac04e6b41c4b2cedf35e80d4db5f9
svyatoslav <svyatoslavvyasovoi@gmail.com>
2025-05-13 12:02:30 +0300

    tabs corrected

--- src/components/RouterTabs.vue
+++ src/components/RouterTabs.vue
@@ -1,11 +1,17 @@
 <script setup>
+import { RouterLink, useRoute, useRouter } from 'vue-router';
+const router = useRouter();
+const route = useRoute();
+console.log(router.getRoutes(), route);
 </script>
 <template>
-    <div class="tabs is-centered">
-        <ul>
-            <li :class="{'is-active': item.path === $route.path}" v-for="(item,key) in $router.getRoutes()">
-                <RouterLink :to="item.path">{{ item.name }}</RouterLink>
-            </li>
-        </ul>
-    </div>
+    <div class="tabs is-toggle is-toggle-rounded is-centered">
+  <ul>
+    <li :class="{'is-active': item.path === $route.path}" v-for="(item, key) in $router.getRoutes()">
+      <RouterLink :to="item.path">
+        <span class="icon is-small"><i class="fas fa-image"> {{ item.name }} </i></span>
+      </RouterLink>
+    </li>
+  </ul>
+</div>
 </template>
