574990e729a5082d01b5c2d589dd2b50d0de8c29
svyatoslav <svyatoslavvyasovoi@gmail.com>
2025-05-13 11:58:10 +0300

    changed

--- src/pages/RickandMorty.vue
+++ src/pages/RickandMorty.vue
@@ -1,38 +1,40 @@
 <script setup>
 import axios from 'axios';
 import { onMounted, ref } from 'vue';
-import CharacterCard from '../components/CharacterCard.vue';
-import PagedPagination from '../components/PagedPagination.vue';
+import CharacterCard from '../comp/CharacterCard.vue';
+import PagedPagination from '../comp/PagedPagination.vue';
 
 let characters = ref([]);
 let pagination = ref({});
 let currentPage = ref(1);
-let searchValue = ref('');
-let searchTimeout = null;
+let searchvalue = ref('');
+let searchTimeout = null
+
 await page(currentPage.value);
 
 async function getCharacters() {
-    let response = await axios.get('https://rickandmortyapi.com/api/character',{
+    let response = await axios.get('https://rickandmortyapi.com/api/character', {
         params: {
             page: currentPage.value,
-            name: searchValue.value
+            name: searchvalue.value
         }
     });
+    console.log(response.data);
     characters.value = response.data.results;
     pagination.value = response.data.info;
 }
 
-async function next() {
+async function next(){
     currentPage.value++;
     await getCharacters();
 }
 
-async function prev() {
+async function prev(){
     currentPage.value--;
     await getCharacters();
 }
 
-async function page(page) {
+async function page(page){
     currentPage.value = page;
     await getCharacters();
 }
@@ -41,15 +43,28 @@ async function search(){
     clearTimeout(searchTimeout);
     searchTimeout = setTimeout(async () => {
         await getCharacters();
-    }, 750);
+    }, 1000);
+    await getCharacters();
 }
+
+// onMounted(() => {
+//     document.addEventListener('scroll', () => {
+//         if(window.scrollY+window.innerHeight > document.body.clientHeight-300) {
+//             if(pagination.value.next){
+//                 next()
+//             }
+//         }
+//     });
+// });
 
 </script>
 
 <template>
+    <PagedPagination :pagination="pagination" :current="currentPage" @next="next" @prev="prev" @page="page"></PagedPagination>
+
     <div class="field has-addons">
         <div class="control is-expanded">
-            <input class="input" type="text" placeholder="Search..." @input="search" v-model="searchValue">
+            <input class="input" type="text" placeholder="Search..." @input="search" v-model="searchvalue">
         </div>
         <div class="control">
             <button class="button is-info" @click="search">
@@ -57,7 +72,6 @@ async function search(){
             </button>
         </div>
     </div>
-    <PagedPagination :current="currentPage" :pagination="pagination" @next="next" @prev="prev" @page="page"></PagedPagination>
     <div class="columns is-multiline">
         <div class="column is-one-quarter" v-for="character in characters">
             <CharacterCard :character="character"></CharacterCard>
