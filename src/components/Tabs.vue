d64681577f182ba003bf0f00ff2dcca28a6e6100
svyatoslav <svyatoslavvyasovoi@gmail.com>
2025-05-13 12:01:32 +0300

    done tabs

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,22 +1,24 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 defineProps(['items']);
 let emit = defineEmits(['change']);
 let active = ref(0);
 
 function setActive(key){
-    active.value = key;
+    active.value=key;
     emit('change', key);
 }
 
 </script>
 <template>
-    <div class="tabs is-centered">
-        <ul>
-            <li :class="{'is-active': active === key}" v-for="(item,key) in items" @click="setActive(key)">
-                <a>{{ item }}</a>
-            </li>
-        </ul>
-    </div>
+    <div class="tabs is-toggle is-toggle-rounded is-centered">
+  <ul>
+    <li :class="{'is-active': active === key}" v-for="(item, key) in items" @click="setActive(key)">
+      <a>
+        <span class="icon is-small"><i class="fas fa-image"> {{ item }} </i></span>
+      </a>
+    </li>
+  </ul>
+</div>
 </template>
