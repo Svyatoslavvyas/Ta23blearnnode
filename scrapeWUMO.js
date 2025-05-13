287adfcbd7b28ed1989454f4c201e7ed097f4326
svyatoslav <svyatoslavvyasovoi@gmail.com>
2025-05-13 14:06:28 +0300

    12.05.25

---
+++ scrapeWUMO.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import * as cheerio from 'cheerio';
+import fs from 'fs';
+import md5 from "md5";
+const sleep = function(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+if(!fs.existsSync('cache')){
+    fs.mkdirSync('cache');
+}
+
+const cacheGet = (name) => {
+    if(fs.existsSync('cache/' + name + '.html')){
+        return fs.readFileSync('cache/' + name + '.html');
+    }
+    return false;
+}
+
+const cacheSet = (name) => {
+    fs.existsSync('cache/' + name + '.html')
+}
+
+let url = mdf = 'https://wumo.com/wumo';
+for (let i=0; i<10; i++){
+    let data = cacheGet(md5(url));
+    if(!data){
+        await sleep(1000);
+        console.log('!!! live data');
+        let res = await axios.get(url);
+        data = res.data;
+        cacheSet(md5(url), data);
+    }
+    const $ = cheerio.load(res.data);
+    let img = $('article.wumo .box-content img');
+    console.log(img.attr('src'), img.attr('alt'));
+    url = prev.attr('href');
+}
